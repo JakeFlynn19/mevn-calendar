a7a78c388be25705a87d34817f4a71998bd85a55
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-05-07 10:41:13 -0400

    server.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './components/router';
-import store from './components/store';
+import router from './router';
+import store from './store';
 import vuetify from './plugins/vuetify';
 import VueTextareaAutosize from 'vue-textarea-autosize';
 
