c58df60b6f051d358a7884d43326e8e573c2127f
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-05-08 21:21:56 -0400

    refactor methods

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -15,11 +15,9 @@
           <v-btn fab text small @click="next">
             <v-icon small>mdi-chevron-right</v-icon>
           </v-btn>
-
           <v-toolbar-title>{{ title }}</v-toolbar-title>
           <div class="flex-grow-1"></div>
           <v-menu bottom right>
-
             <template v-slot:activator="{ on }">
               <v-btn outlined v-on="on">
                 <span>{{ typeToLabel[type] }}</span>
@@ -61,62 +59,89 @@
         </v-card>
       </v-dialog>
 
-      <v-sheet height="600">
-        <v-calendar
-          ref="calendar"
-          v-model="focus"
-          color="primary"
-          :events="events"
-          :event-color="getEventColor"
-          :event-margin-bottom="3"
-          :now="today"
-          :type="type"
-          @click:event="showEvent"
-          @click:more="viewDay"
-          @click:date="viewDay"
-          @change="updateRange"
-        ></v-calendar>
-        <v-menu
-          v-model="selectedOpen"
-          :close-on-content-click="false"
-          :activator="selectedElement"
-          full-width
-          offset-x
-        >
-          <v-card color="grey lighten-4" :width="350" flat>
-            <v-toolbar :color="selectedEvent.color" dark>
-              <v-btn @click="deleteEvent(selectedEvent._id)" icon>
-                <v-icon>mdi-delete</v-icon>
+      <v-dialog v-model="dialogDate" max-width="500">
+        <v-card>
+          <v-container>
+            <v-form @submit.prevent="addEvent">
+              <v-text-field v-model="name" type="text" label="event name (required)"></v-text-field>
+              <v-text-field v-model="details" type="text" label="detail"></v-text-field>
+              <v-text-field v-model="start" type="date" label="start (required)"></v-text-field>
+              <v-text-field v-model="end" type="date" label="end (required)"></v-text-field>
+              <v-text-field v-model="color" type="color" label="color (click to open color menu)"></v-text-field>
+              <v-btn type="submit" color="primary" class="mr-4" @click.stop="dialog = false">
+                create event
               </v-btn>
-              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-              <div class="flex-grow-1"></div>
-            </v-toolbar>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-dialog>
 
-            <v-card-text>
-              <textarea-autosize
-                v-model="selectedEvent.details"
-                readonly
-                type="text"
-                style="width: 100%; outline:0px !important;"
-                :min-height="100"
-                placeholder="add note">
-              </textarea-autosize>
-            </v-card-text>
+<v-sheet height="600">
+  <v-calendar
+  ref="calendar"
+  v-model="focus"
+  color="primary"
+  :events="events"
+  :event-color="getEventColor"
+  :event-margin-bottom="3"
+  :now="today"
+  :type="type"
+  @click:event="showEvent"
+  @click:more="viewDay"
+  @click:date="setDialogDate"
+  @change="updateRange"
+  ></v-calendar>
+  <v-menu
+  v-model="selectedOpen"
+  :close-on-content-click="false"
+  :activator="selectedElement"
+  full-width
+  offset-x
+  >
+  <v-card color="grey lighten-4" :width="350" flat>
+    <v-toolbar :color="selectedEvent.color" dark>
+      <v-btn @click="deleteEvent(selectedEvent.id)" icon>
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+      <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+      <div class="flex-grow-1"></div>
+    </v-toolbar>
 
-            <v-card-actions>
-              <v-btn text color="secondary" @click="selectedOpen = false">
-                close
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-menu>
-      </v-sheet>
-    </v-col>
-  </v-row>
+    <v-card-text>
+      <form v-if="currentlyEditing !== selectedEvent.id">
+        {{ selectedEvent.details }}
+      </form>
+      <form v-else>
+        <textarea-autosize
+        v-model="selectedEvent.details"
+        type="text"
+        style="width: 100%"
+        :min-height="100"
+        placeholder="add note">
+      </textarea-autosize>
+    </form>
+  </v-card-text>
+
+  <v-card-actions>
+    <v-btn text color="secondary" @click="selectedOpen = false">
+      close
+    </v-btn>
+    <v-btn v-if="currentlyEditing !== selectedEvent.id" text @click.prevent="editEvent(selectedEvent)">
+      edit
+    </v-btn>
+    <v-btn text v-else type="submit" @click.prevent="updateEvent(selectedEvent)">
+      Save
+    </v-btn>
+  </v-card-actions>
+</v-card>
+</v-menu>
+</v-sheet>
+</v-col>
+</v-row>
 </template>
 
 <script>
-import axios from 'axios'
+import { db } from '@/main'
 export default {
   data: () => ({
     today: new Date().toISOString().substr(0, 10),
@@ -128,7 +153,6 @@ export default {
       day: 'Day',
       '4day': '4 Days',
     },
-    id: '',
     name: null,
     details: null,
     start: null,
@@ -139,7 +163,8 @@ export default {
     selectedElement: null,
     selectedOpen: false,
     events: [],
-    dialog: false
+    dialog: false,
+    dialogDate: false
   }),
   mounted () {
     this.getEvents()
@@ -160,12 +185,12 @@ export default {
       const endDay = end.day + this.nth(end.day)
       switch (this.type) {
         case 'month':
-          return `${startMonth} ${startYear}`
+        return `${startMonth} ${startYear}`
         case 'week':
         case '4day':
-          return `${startMonth} ${startDay} ${startYear} - ${suffixMonth} ${endDay} ${suffixYear}`
+        return `${startMonth} ${startDay} ${startYear} - ${suffixMonth} ${endDay} ${suffixYear}`
         case 'day':
-          return `${startMonth} ${startDay} ${startYear}`
+        return `${startMonth} ${startDay} ${startYear}`
       }
       return ''
     },
@@ -176,12 +201,19 @@ export default {
     }
   },
   methods: {
-    getEvents () {
-      axios({ method: 'GET', url: 'http://localhost:3000/tasks' }).then(
-        result => {
-          this.events = result.data
-        }
-      )
+    async getEvents () {
+      let snapshot = await db.collection('calEvent').get()
+      const events = []
+      snapshot.forEach(doc => {
+        let appData = doc.data()
+        appData.id = doc.id
+        events.push(appData)
+      })
+      this.events = events
+    },
+    setDialogDate( { date }) {
+      this.dialogDate = true
+      this.focus = date
     },
     viewDay ({ date }) {
       this.focus = date
@@ -199,34 +231,39 @@ export default {
     next () {
       this.$refs.calendar.next()
     },
-    addEvent () {
+    async addEvent () {
       if (this.name && this.start && this.end) {
-        axios.post('http://localhost:3000/task',
-          {
-            name: this.name,
-            details: this.details,
-            start: this.start,
-            end: this.end,
-            color: this.color
-          }
-        ).then(() => {
-          this.getEvents()
-          this.name = '',
-          this.details = '',
-          this.start = '',
-          this.end = '',
-          this.color = ''
+        await db.collection("calEvent").add({
+          name: this.name,
+          details: this.details,
+          start: this.start,
+          end: this.end,
+          color: this.color
         })
+        this.getEvents()
+        this.name = '',
+        this.details = '',
+        this.start = '',
+        this.end = '',
+        this.color = ''
       } else {
         alert('You must enter event name, start, and end time')
       }
     },
-    deleteEvent (id) {
-      axios.delete(`http://localhost:3000/task/${id}`
-      ).then(() => {
-        this.selectedOpen = false,
-        this.getEvents()
+    editEvent (ev) {
+      this.currentlyEditing = ev.id
+    },
+    async updateEvent (ev) {
+      await db.collection('calEvent').doc(this.currentlyEditing).update({
+        details: ev.details
       })
+      this.selectedOpen = false,
+      this.currentlyEditing = null
+    },
+    async deleteEvent (ev) {
+      await db.collection("calEvent").doc(ev).delete()
+      this.selectedOpen = false,
+      this.getEvents()
     },
     showEvent ({ nativeEvent, event }) {
       const open = () => {
@@ -248,29 +285,9 @@ export default {
     },
     nth (d) {
       return d > 3 && d < 21
-        ? 'th'
-        : ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'][d % 10]
+      ? 'th'
+      : ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'][d % 10]
     }
   }
 }
 </script>
-
-<style media="screen">
-body {
-  margin: 0;
-  padding: 0;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin: 0;
-  padding: 0;
-}
-.card-container {
-  margin: 10px;
-  padding: 10px;
-  min-height: 200px;
-}
-</style>
