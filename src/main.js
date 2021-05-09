0492dfe1d3c5753cf6cd5e8afb139c916db5d44b
Jake Flynn <JakeFlynn@Jakes-MBP-4.attlocal.net>
2021-05-08 20:50:00 -0400

    connect to firestore

--- src/main.js
+++ src/main.js
@@ -1,17 +1,25 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from "firebase";
+import VueTextareaAutosize from 'vue-textarea-autosize'
 
 Vue.use(VueTextareaAutosize)
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyAYXrFecJmTqCyjJMbwoslFTBkx6v9N79Y",
+  authDomain: "vue-cal-e4bc7.firebaseapp.com",
+  projectId: "vue-cal-e4bc7",
+  storageBucket: "vue-cal-e4bc7.appspot.com",
+  messagingSenderId: "727976140262",
+  appId: "1:727976140262:web:9376a3880dccf8f88c4a27"
+});
+
+export const db = firebase.firestore();
+
 new Vue({
-  router,
-  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
